c034099ff0df4d71c0d3ac0c7455834866d5be3f
Ishan Jain <contact@ishanjain.me>
2024-06-08 09:04:46 +0530

    improved repl with a quit instruction

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,34 @@ mod scanner;
 mod loxi;
 mod repl;
 
-use std::env;
+use std::{env, fs::File, io::Read, process};
+
+use loxi::run;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     match args.len() {
-        1 => repl::init(),
-        v if v > 1 => {
-            println!("Usage: loxi [script]")
+        2 => {
+            println!("{:?}", args);
+            run_file(&args[1]);
+        }
+        v if v > 2 => {
+            println!("Usage: loxi [script]");
+            process::exit(64);
         }
         _ => {
-            // TODO: Read the file
+            repl::init();
         }
     }
+}
+
+fn run_file(f: &str) {
+    let mut f = File::open(f).expect("error in opening file");
+    let mut contents = Vec::new();
+    f.read_to_end(&mut contents).expect("error in reading file");
+
+    let contents = String::from_utf8(contents).expect("file is not encoded in utf8");
+
+    run(&contents)
 }
--- src/repl.rs
+++ src/repl.rs
@@ -1,5 +1,9 @@
 use crate::loxi::run;
-use std::io::{self, BufRead, Write};
+use std::{
+    io::{self, BufRead, Write},
+    sync::mpsc,
+    time::Duration,
+};
 
 const PROMPT: &[u8] = b">> ";
 
@@ -13,9 +17,29 @@ pub fn init() {
 }
 
 fn start<R: BufRead, W: Write>(mut ip: R, mut out: W) {
+    let (send, recv) = mpsc::channel();
+
+    let mut should_quit = false;
+    ctrlc::set_handler(move || {
+        send.send(()).expect("error in sending signal to channel");
+    })
+    .expect("error in setting Ctrl+C handler");
+
     loop {
         out.write_all(PROMPT).unwrap();
         out.flush().unwrap();
+        if recv.recv_timeout(Duration::from_millis(5)).is_ok() {
+            if should_quit {
+                std::process::exit(0);
+            }
+            should_quit = true;
+            out.write_all(b"\r                                                          \r")
+                .unwrap();
+            continue;
+        } else {
+            should_quit = false;
+        }
+
         let mut s = String::new();
         ip.read_line(&mut s).unwrap();
 
